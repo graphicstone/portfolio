e6a355f7cf9c740b083a5300ab77a156fba73ba0
graphicstone <harishiv8@gmail.com>
2024-06-02 17:04:33 +0530

    Drawer added for mobile screen

--- src/layouts/landing-page/components/toolbar/Toolbar.jsx
+++ src/layouts/landing-page/components/toolbar/Toolbar.jsx
@@ -4,9 +4,37 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import { toolbarStyles } from './ToolbarStyles.js';
 import Menu from '../../../../assets/svg/ic_menu.svg?react';
+import { Drawer, List, ListItem, ListItemButton } from '@mui/material';
 
 export default function Toolbar() {
   const toolbarItems = ['About Me', 'Skills', 'Project', 'Contact Me'];
+  const [drawerState, setDrawerState] = React.useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setDrawerState(open);
+  };
+
+  const drawerList = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {toolbarItems.map((text) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <Typography variant="paragraph_p2_regular">{text}</Typography>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <Box id="toolbar" sx={toolbarStyles.container}>
@@ -24,9 +52,20 @@ export default function Toolbar() {
         sx={{ display: { xs: 'none', md: 'flex' } }}>
         Resume
       </Button>
-      <Box id="toolbar-menu" sx={{ display: { xs: 'flex', md: 'none' } }}>
+      <Box
+        id="toolbar-menu"
+        sx={{ display: { xs: 'flex', md: 'none' } }}
+        onClick={toggleDrawer(true)}
+      >
         <Menu />
       </Box>
+      <Drawer
+        anchor="right"
+        open={drawerState}
+        onClose={toggleDrawer(false)}
+      >
+        {drawerList()}
+      </Drawer>
     </Box>
   );
 }
