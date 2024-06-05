1c851f5b9e636347ae8aea9bcd9b4b01baf663c9
graphicstone <harishiv8@gmail.com>
2024-06-05 21:55:46 +0530

    console logs added

--- src/layouts/firebase.js
+++ src/layouts/firebase.js
@@ -12,6 +12,9 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
+console.log('firebaseConfig', firebaseConfig);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+console.log('app', app);
 export const db = getFirestore(app);
