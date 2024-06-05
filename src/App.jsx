02813f5da56de433a7e150f218b442a670aa94bb
graphicstone <harishiv8@gmail.com>
2024-06-05 22:48:39 +0530

    Vercel analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import LandingPage from './layouts/landing-page/LandingPage.jsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <>
       <LandingPage />
       <ToastContainer />
+      <Analytics />
     </>
   );
 }
